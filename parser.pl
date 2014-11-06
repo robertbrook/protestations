3be97c92a3f33ee3599ac53bdad52a8af430724a
Robert Brook <robertbrook@fastmail.fm>
2014-11-06 23:22:55 +0000

    tired

--- parser.pl
+++ parser.pl
@@ -1,10 +1,11 @@
- #!/usr/bin/perl
+#!/usr/bin/perl
 
 use Modern::Perl;
 use Text::CSV;
 use Data::Dumper;
 use IO::All -utf8;
-use HTTP::Tiny;
+use LWP::Simple;
+use JSON;
 
 my $csv = Text::CSV->new();
 
@@ -12,24 +13,25 @@ my $filename = 'records.csv';
 
 my $lines = io $filename;
 
-while ( my $row = $csv->getline($lines) ) {
-    
-    my $title = $row->[1];
+while ( my $row = $csv->getline($lines)) {
+	
+    my $title               = $row->[1];
     my $catalogue_reference = $row->[0];
-    my @title_parts = reverse split( " - ", $title );
-    
-# 	say join ' --- ', @title_parts;
-	my $nice_first = @title_parts[0];
-	$nice_first =~ s/\.$//;
-	say $nice_first;
+    my @title_parts         = reverse split( " - ", $title );
 
-        
-    ## end if ( defined $bits[4] )
+    # 	say join ' --- ', @title_parts;
 
-} ## end while ( my $row = $csv->getline...)
+    my $nice_first = $title_parts[0];
+    $nice_first =~ s/\.$//;       # trailing period
+    $nice_first =~ s/^\s//;       # leading space
+    $nice_first =~ s/\[\?\]//;    # leading space
+    say "";
+    say $nice_first;
 
- # my $response = HTTP::Tiny->new->get(
-# 'http://data.ordnancesurvey.co.uk/datasets/os-linked-data/apis/search?query='
-#               . $target );
-# say $response->{content};
-#         say "";
+    my $body = get(
+'http://data.ordnancesurvey.co.uk/datasets/os-linked-data/apis/search?query='
+          . $nice_first );
+    say $body;
+# 	say Dumper($body);
+	}
+
